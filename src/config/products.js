80da33b74f9ff0ed38c36984bea919ca638fb873
Guruprasad037 <guruprasad037@gmail.com>
2026-06-03 14:37:00 +0530

    changing prices to Rs 1 for testin

--- src/config/products.js
+++ src/config/products.js
@@ -10,22 +10,22 @@ const PRODUCTS = {
   career: {
     id:           'career',
     name:         'Career & Wealth Report',
-    amount_paise: 59900,
+    amount_paise: 100,
   },
   love: {
     id:           'love',
     name:         'Love & Relationships Report',
-    amount_paise: 59900,
+    amount_paise: 100,
   },
   blueprint: {
     id:           'blueprint',
     name:         'Full Life Blueprint',
-    amount_paise: 99900,
+    amount_paise: 100,
   },
   health: {
     id:           'health',
     name:         'Health & Wellbeing Report',
-    amount_paise: 49900,
+    amount_paise: 100,
   },
 };
 
